533d3d47b938b4bd54285cf87f63b396b218f472
mol1986@naver.com <mol1986@naver.com>
2022-03-21 19:49:03 +0900

    Update

--- Game.c/Source.c
+++ Game.c/Source.c
@@ -1,115 +1,82 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <windows.h>
-
-void Move(int x, int y)
-{
-	// x,y 좌표 설정
-	COORD position = {x,y};
-
-	// 커서를 이동하는 함수
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
-}
 
 int main()
 {
-	// 중복이 되는 난수
-	/*
-	int value = 0;
 	
-	// time(NULL) : 1970년 1월 1일 이후에 경과된 시간을 초 단위로 반환하는 함수입니다.
-	srand(time(NULL));
+	// 증감 연산자
+/*
+int number_1 = 10;
+int number_2 = 10;
 
-	for (int i = 0; i < 10; i++)
-	{
-		// rand() : 0 ~ 32767의 수를 반환하는 함수입니다.
-		value = rand() % 10 + 1;
-		printf("%d\n", value);
-	}
-	*/
+// ++가 변수 앞에 오게되면 먼저 연산을 수행한 후에 값을 증가시킵니다.
+printf("전위 증가 연산자 : %d\n", ++number_1);
 
-	// 중복되지 않는 난수
+// --가 변수 앞에 오게되면 먼저 연산을 수행한 후에 값을 감소시킵니다.
+printf("전위 감소 연산자 : %d\n", --number_2);
+
+// 변수 뒤에 ++가 오게되면 나중에 값을 증가시키고 연산을 수행합니다.
+printf("후위 증가 연산자 : %d\n", number_1++);
+
+// 변수 뒤에 --가 오게되면 나중에 값을 감소시키고 연산을 수행합니다.
+printf("후위 감소 연산자 : %d\n", number_2--);
+
+printf("number_1의 값 : %d\n", number_1);
+printf("number_2의 값 : %d\n", number_2);
+*/
+
+// while 문
+/*
+int person = 3;
+
+while (person > 2) //
+{
+	printf("공장의 인원을 재 배치해주세요.\n");
+	scanf_s("%d", &person);
+}
+*/
+
+// do while 문
+/*
+do
+{
+	printf("조건이 틀렸지만, 실행됩니다.");
+}
+while (3 > 2);
+*/
+
+// for 문
+/*
+int value = 1;
+
+for (int i = 0; i <= 5; i++)
+{
+	printf("%d", value);
+}
+*/
+
+// 1 ~ 10까지의 합을 구해주세요.
+//int value = 0;
+
+//for (int i = 1; i <= 10; i++)
+//{
+//	value += i;
+//}
+
+//printf("1 ~ 10 까지의 합 : %d", value);
+
+// 구구단 
 	/*
-	int array[10] = { 0, };
-
-	for(int i = 0; i < 10; i++)
+	for (int i = 1; i < 10; i++)
 	{
-		array[i] = rand() % 10 + 1;
-
-		for (int j = 0; j < i; j++)
+		for (int j = 1; j < 10; j++)
 		{
-			if (array[j] == array[i])
-			{
-				i--;
-			}
+			printf("%d * %d = %d\n", i, j, i * j);
 		}
-	}
 
-	for (int i = 0; i < 10; i++)
-	{
-		printf("%d\n", array[i]);
+		printf("\n");
 	}
 	*/
 
-	// 파일 쓰기
-	/*
-	FILE * file = fopen("file.txt", "w");
-
-	fputs("flie\n", file);
-	fputs("1000 won\n", file);
-
-	fclose(file);
-	*/
-
-	// 파일 읽기
-	/*
-	char buffer[10000] = { 0, }; // 파일을 읽을 때 사용할 임시 공간이며, 0으로 초기화합니다.
-
-	FILE * file = fopen("Cat.txt", "r");
-
-	// buffer = char 배열
-	// 10000 = 읽어들일 원소의 크기로 단위는 바이트이며, size가 4 이면 하나의 원소의 크기는 4 바이트이다.
-    // 1 = 크기를 가지는 배열을 가리키는 포인터 
-	// file = 데이터를 입력받을 스트림의 FILE 객체를 가리키는 포인터
-
-	fread(buffer, 1,sizeof(buffer), file); 
-
-	printf("%s", buffer);
-
-	fclose(file);
-	*/
-
-	int x = 5;
-	int y = 5;
-
-	// 동기화 : 다음 작업으로 넘어가기 위해 현재 하고 있는 작업을 다 끝내야 다음 작업으로 넘어갈 수 있는 것입니다.
-	// 비동기화 : 다음 작업으로 넘어가기 위해 현재 하고 있는 작업 이외에 다른 작업을 하면서 넘어갈 수 있는 것입니다.
-
-	while (1)
-	{
-		if (GetAsyncKeyState(VK_UP))
-		{
-			y--;
-		}
-		if (GetAsyncKeyState(VK_DOWN))
-		{
-			y++;
-		}
-		if (GetAsyncKeyState(VK_LEFT))
-		{
-			x--;
-		}
-		if (GetAsyncKeyState(VK_RIGHT))
-		{
-			x++;
-		}
-
-		system("cls");
-		Move(x, y);
-		printf("☆");
-	}
 
 	return 0;
 }
