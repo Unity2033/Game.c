af27f22372f99b7f4669f12dc8c850c7d58041c4
mol1986@naver.com <mol1986@naver.com>
2022-03-23 19:47:44 +0900

    For Statement Star

--- 2022.3.22.h
+++ 2022.3.22.h
@@ -25,3 +25,22 @@ memory >>= 5;
 
 printf("%d", memory); // 0000 0000
 */
+
+// 최대 공약수 
+/*
+int value1 = 16;
+int value2 = 20;
+
+int result = 0;
+
+for (int i = 1; i <= value1 && i <= value2; i++)
+{
+	if (value1 % i == 0 && value2 % i == 0)
+	{
+		result = i;
+	}
+}
+
+
+printf("%d", result);
+*/
--- Game.c/Source.c
+++ Game.c/Source.c
@@ -2,66 +2,59 @@
 
 int main()
 {
-	// continue 문
-    /*
-	for (int i = 1; i <= 10; i++)
-	{
-		if (i % 3 == 0)
-		{
-			continue;
-		}
-
-		printf("%d\n", i);
-	}
-	*/
-
-	// 쉬프트 연산자
+	// 포인터
 	/*
-	int memory = 3; // 0000 0011
-
-	// 비트를 3번 오른쪽으로 이동시켰습니다.
-	printf("%d\n", memory <<= 3); // 0001 1000
-
-	// 비트를 5번 왼쪽으로 이동시켰습니다.
-	memory >>= 5;
-	
-	printf("%d", memory); // 0000 0000
-	*/
-
-	// 최대 공약수 
-	/*
-	int value1 = 16;
-	int value2 = 20;
-
-	int result = 0;
-
-	for (int i = 1; i <= value1 && i <= value2; i++)
-	{
-		if (value1 % i == 0 && value2 % i == 0)
-		{
-			result = i;
-		}
-	}
-	
-
-	printf("%d", result);
-	*/
-
+    // value라는 메모리 공간을 생성했습니다.
 	int value = 10; 
 
-	printf("%p\n", &value);
+	int a = 50;
 
-	// 포인터 변수도 자체적으로 메모리 공간을 가지고 있습니다.
+	// 포인터의 크기는 4 byte입니다.
+	// CPU와 운영체제에 따라 4 ~ 8 byte의 크기를 가질 수 있습니다.
 	int * ptr = &value;
 
-	// ptr  = value의 시작주소를 가리키는 것입니다.
-	// &ptr = ptr의 메모리 주소가 출력되는 거에요.
+	printf("포인터 변수의 값 : %d\n", *ptr); 
 
-	printf("%p", &ptr);
+	ptr = &a;
 
-	*ptr = 2000;
+	* ptr = 1000;
 
-	printf("%d", value);
+	printf("포인터 변수의 값 : %d\n", *ptr); 
+
+	printf("포인터의 크기 : %d", sizeof(ptr));
+	*/
+
+	// 이중 포인터
+	/*
+	int x = 0;
+
+	int * ptr = &x;
+
+	int ** pptr = &ptr;
+
+	printf("변수 x의 주소 : %p\n", &x);
+	printf("ptr의 주소 : %p\n", &ptr);
+	printf("pptr의 주소 : %p\n", &pptr);
+
+	printf("pptr의 값 : %d", **pptr);
+	*/
+
+
+	// 초기식을 설정합니다. ( int i = 0 )
+	// 조건식을 설정합니다. ( i < 5 )
+
+	/*
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			printf("*");
+		}
+
+		printf("\n");
+	}
+	*/
+
 
 	return 0;
 }
