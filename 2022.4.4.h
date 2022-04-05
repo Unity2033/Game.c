5f84364f1aa962663eed84a6f83f551ed5f6edba
mol1986@naver.com <mol1986@naver.com>
2022-04-05 19:48:05 +0900

    Dynamic Class

---
+++ 2022.4.4.h
@@ -0,0 +1,48 @@
+#pragma once
+// 재귀 함수
+/*
+int Function(int num)
+{
+	if (num < 1)
+	{
+		return 1;
+	}
+	else
+	{
+		printf("Function\n");
+		Function(num - 1);
+	}
+}
+*/
+
+// 팩토리얼
+/*
+int Factorial(int num)
+{
+	if (num <= 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return num * Factorial(num - 1);
+	}
+}
+*/
+
+// main
+
+	// 재귀 함수
+	/*
+	int value = Function(5); // 스택 메모리 영역에 올라갔다가 종료가 됩니다.
+
+	printf("Function의 값 : %d", value);
+	*/
+
+	// 팩토리얼
+	/*
+	int result = Factorial(4);
+
+	printf("Factorial의 값 : %d", result);
+	*/
+
