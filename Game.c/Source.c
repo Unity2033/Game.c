7fc4dc5f666d349e94eabc8dd88f7ffe8d03c92d
mol1986@naver.com <mol1986@naver.com>
2022-04-18 19:47:20 +0900

    Maze Map Create

--- Game.c/Source.c
+++ Game.c/Source.c
@@ -3,6 +3,14 @@
 #include <conio.h>
 #include <windows.h>
 
+int count = 2; 
+
+enum Stage 
+{
+	First = 1, 
+	Second, 
+	Three
+};
 
 int map[10][10] = 
 {
@@ -18,18 +26,43 @@ int map[10][10] =
 	{1,1,1,1,1,1,1,1,1,1}
 };
 
+int normal[10][10] =
+{
+	{1,1,1,1,1,1,1,1,1,1},
+	{1,0,0,1,1,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,1,1,1,1,0,1},
+	{1,1,1,0,0,0,0,1,0,1},
+	{1,0,0,0,0,0,0,1,0,1},
+	{1,1,1,1,0,1,1,1,0,1},
+	{1,1,0,1,0,1,0,1,0,1},
+	{1,0,0,0,0,0,0,0,0,1},
+	{1,1,1,1,1,1,1,1,1,1}
+};
+
 void gotoxy(int x, int y)
 {
 	COORD pos = { x,y }; // x, y 좌표 설정
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos); // 좌표 위치 이동 
 }
 
-void Village() 
+void Render() 
 {
 	for (int i = 0; i < 10; i++)
 	{
 		for (int j = 0; j < 10; j++) 
 		{
+			switch (count)
+			{
+			case First:
+				map[j][i];
+				break;
+			case Second : map[j][i] = normal[j][i];
+				break;
+			case Three:		
+				break;
+			}
+
 			if (map[j][i] == 1)
 			{				
 				printf("■");
@@ -45,6 +78,7 @@ void Village()
 }
 
 
+
 int main()
 {
 	int key; 
@@ -52,15 +86,25 @@ int main()
 
 	while (1)
 	{	
-		Village();
+		switch (count)
+		{
+		    case First :
+				Render();
+				break;
+			case Second:
+				Render();
+				break;
+			case Three:
+				Render();
+				break;
+		}
 
 		gotoxy(x, y);
-		
 		printf("★");
-	
-		key = _getch(); 
 
-		switch (key)
+		key = _getch(); // 키 입력을 받았을 때 key에 값(스캔코드)을 넣습니다.
+
+		switch (key) // key에 저장된 값에 따라 키 입력 처리를 하였습니다.
 		{
 			case 72 :
 				if (map[x / 2][y-1] == 1) break; 
@@ -71,7 +115,7 @@ int main()
 				x -= 2;
 				break;
 			case 77 : 
-				if (map[x / 2 + 1][y] == 1) break;	
+				if (map[x / 2 + 1][y] == 1)	break;			
                 x += 2; 
 				break; 
 			case 80 : 
@@ -80,18 +124,6 @@ int main()
 				break;
 			default:
 				break;
-		}
-
-		// 웹 사이트 이동 
-		if (GetAsyncKeyState(VK_BACK))
-		{
-			system("start https://www.naver.com/");
-		}
-
-		//// 일시 정지 
-		if (GetAsyncKeyState(VK_RETURN))
-		{
-			system("pause");
 		}
 
 		system("cls");
